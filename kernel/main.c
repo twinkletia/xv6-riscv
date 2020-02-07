8209cd852155b4b6b5cedccde5801d230a6b7c9e
Takashi Yoneuchi <takashi.yoneuchi@shift-js.info>
2020-02-07 10:20:51 +0900

    add debug print for uart

--- kernel/main.c
+++ kernel/main.c
@@ -16,18 +16,31 @@ main()
     printf("\n");
     printf("xv6 kernel is booting\n");
     printf("\n");
+    printf("[*] kinit\n");
     kinit();         // physical page allocator
+    printf("[*] kvminit\n");
     kvminit();       // create kernel page table
+    printf("[*] kvminithart\n");
     kvminithart();   // turn on paging
+    printf("[*] procinit\n");
     procinit();      // process table
+    printf("[*] trapinit\n");
     trapinit();      // trap vectors
+    printf("[*] trapinithart\n");
     trapinithart();  // install kernel trap vector
+    printf("[*] plicinit\n");
     plicinit();      // set up interrupt controller
+    printf("[*] plicinithart\n");
     plicinithart();  // ask PLIC for device interrupts
+    printf("[*] binit\n");
     binit();         // buffer cache
+    printf("[*] iinit\n");
     iinit();         // inode cache
+    printf("[*] fileinit\n");
     fileinit();      // file table
+    printf("[*] virtio\n");
     virtio_disk_init(); // emulated hard disk
+    printf("[*] userinit\n");
     userinit();      // first user process
     __sync_synchronize();
     started = 1;
